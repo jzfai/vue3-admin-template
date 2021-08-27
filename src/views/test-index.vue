d70443011b025dff4b6c6dc90f988d01dba7e518
邝华 <kuanghua@ruixiude.com>
2021-08-27 13:45:14 +0800

    添加preview:build script

--- src/views/test-index.vue
+++ src/views/test-index.vue
@@ -2,6 +2,7 @@
   svg icon
   <div>
     <svg-icon icon-class="assign" class="svg-icon" />
+    <el-button type="primary">fai</el-button>
   </div>
   <!--<el-button type="primary">element-btn1111111111111111111111111</el-button>-->
 </template>
