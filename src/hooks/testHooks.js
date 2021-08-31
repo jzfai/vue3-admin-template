a351203d07e826eb878004c0cc9c6d3b6e19e3f2
kuanghua <869653722@qq.com>
2021-08-31 17:53:20 +0800

    add vue3-template

--- src/hooks/testHooks.js
+++ src/hooks/testHooks.js
@@ -1,12 +1,11 @@
 //https://blog.csdn.net/weixin_34865745/article/details/113992767
 import { onMounted, ref } from 'vue'
 export default function () {
-  const ref1 = ref(-1)
-  const ref2 = ref(-1)
-  onMounted(() => {
-    console.log('hooks执行了')
-  })
+  const ref1 = ref('hook数据')
+  const ref2 = ref('hook数据ref2')
+  onMounted(() => {})
   const hooksFunc = () => {
+    alert('我是hook里的方法')
     return 'hook方法执行了'
   }
   return {
