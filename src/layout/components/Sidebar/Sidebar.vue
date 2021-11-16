ed8795d61abbd6a833640d608047d6d5da4288e9
kuanghua <869653722@qq.com>
2021-11-16 11:58:47 +0800

    fix left menu animation

--- src/layout/components/Sidebar/Sidebar.vue
+++ src/layout/components/Sidebar/Sidebar.vue
@@ -5,7 +5,7 @@
     <!--router nav-->
     <el-scrollbar>
       <el-menu
-          class="el-menu-vertical"
+        class="el-menu-vertical"
         :default-active="activeMenu"
         :collapse="!isCollapse"
         :unique-opened="false"
