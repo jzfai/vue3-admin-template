a2ad01dbc5e8efbd4569c90bccfbdf6eefab4ba7
jzfai <869653722@qq.com>
2021-12-13 16:21:19 +0800

    fix some issue and update version

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,16 @@ import viteSvgIcons from 'vite-plugin-svg-icons'
 //mock
 import { viteMockServe } from 'vite-plugin-mock'
 import setting from './src/settings'
-import { loadEnv } from 'vite'
-console.log('import.meta.env')
+import packageJson from './package.json'
+// import { loadEnv } from 'vite'
 const prodMock = setting.openProdMock
 export default ({ command, mode }) => {
   return {
-    base: '/', //default
+    /*
+     * "/vue3-admin-template" nginx deploy folder
+     * detail to look https://vitejs.cn/config/#base
+     * */
+    base: `/${packageJson.name}/`,
     define: {
       'process.platform': null,
       'process.version': null
