e4161622d28572706ff6949ba78a5acb91ee867e
jzfai <869653722@qq.com>
2022-02-11 17:51:56 +0800

    add third router keep-alive func

---
+++ src/views/example/keep-alive/deep-children/DeepChildrenSd.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="mt-2">DeepChildrenSd.vue third router render</div>
+  <el-input v-model="testInput" placeholder="input test keepAlive(DeepChildren)" class="widthPx-300" />
+</template>
+
+<script setup name="DeepChildrenSd">
+const $store = useStore()
+let testInput = ref('')
+//$store.commit('app/M_ADD_CACHED_VIEW_DEEP', 'DeepChildren')
+</script>
+
+<style scoped lang="scss"></style>
