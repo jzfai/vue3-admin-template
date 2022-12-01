c9fa9e91a5e37e031d1d20f25323cb5026b53135
KHFai <1319404169@qq.com>
2022-12-01 18:28:01 +0800

    add proxy demo

--- typings/env.d.ts
+++ typings/env.d.ts
@@ -3,6 +3,8 @@ declare global {
     readonly VITE_APP_BASE_URL: string
     readonly VITE_APP_IMAGE_URL: string
     readonly VITE_APP_ENV: string
+    readonly VITE_PROXY_BASE_URL: string
+    readonly VITE_PROXY_URL: string
     // 更多环境变量...
   }
   interface ImportMeta {
