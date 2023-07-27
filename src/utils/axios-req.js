beff80cacb5ff3240edeabdca58f6e9f800ccd7f
jzfai <869653722@qq.com>
2023-07-27 15:53:32 +0800

    fix element-plus version issue

--- src/utils/axios-req.js
+++ src/utils/axios-req.js
@@ -6,6 +6,22 @@ import { useBasicStore } from '@/store/basic'
 const service = axios.create()
 let loadingInstance = null //loading实例
 let tempReqUrlSave = ''
+let authorTipDoor = true
+
+const noAuthDill = () => {
+  authorTipDoor = false
+  ElMessageBox.confirm('请重新登录', {
+    confirmButtonText: '重新登录',
+    closeOnClickModal: false,
+    showCancelButton: false,
+    showClose: false,
+    type: 'warning'
+  }).then(() => {
+    useBasicStore().resetStateAndToLogin()
+    authorTipDoor = true
+  })
+}
+
 //请求前拦截
 service.interceptors.request.use(
   (req) => {
@@ -18,10 +34,11 @@ service.interceptors.request.use(
         cancel
       })
     })
+
     //设置token到header
-    req.headers['AUTHORIZE_TOKEN'] = token
+    if (token) req.headers['Authorization'] = token
     //如果req.method给get 请求参数设置为 ?name=xxx
-    if ('get'.includes(req.method?.toLowerCase())) req.params = req.data
+    if ('get'.includes(req.method?.toLowerCase()) && !req.params) req.params = req.data
 
     //req loading
     // @ts-ignore
@@ -31,7 +48,7 @@ service.interceptors.request.use(
         fullscreen: true,
         // spinner: 'CircleCheck',
         text: '数据载入中...',
-        background: 'rgba(0, 0, 0, 0.3)'
+        background: 'rgba(0, 0, 0, 0.1)'
       })
     }
     return req
@@ -44,44 +61,52 @@ service.interceptors.request.use(
 //请求后拦截
 service.interceptors.response.use(
   (res) => {
-    useBasicStore().remotePromiseArrByReqUrl(tempReqUrlSave) //从请求集合中移除
+    //取消请求
+    useBasicStore().remotePromiseArrByReqUrl(tempReqUrlSave)
     if (loadingInstance) {
       loadingInstance && loadingInstance.close()
     }
     //download file
-    if (['application/zip', 'zip', 'blob', 'arraybuffer'].includes(res.headers['content-type'])) {
-      return res
+    const noAuthCode = '401,403'
+    if (res.data?.size) {
+      new Response(res.data).text().then((stringData) => {
+        const parseJson = JSON.parse(stringData)
+        if (noAuthCode.includes(parseJson.code)) {
+          noAuthDill()
+          return
+        } else {
+          return res
+        }
+      })
     }
     const { code, msg } = res.data
     const successCode = '0,200,20000'
-    const noAuthCode = '401,403'
     if (successCode.includes(code)) {
       return res.data
     } else {
-      if (noAuthCode.includes(code)) {
-        ElMessageBox.confirm('请重新登录', {
-          confirmButtonText: '重新登录',
-          closeOnClickModal: false,
-          showCancelButton: false,
-          showClose: false,
-          type: 'warning'
-        }).then(() => {
-          useBasicStore().resetStateAndToLogin()
-        })
+      //authorTipDoor 防止多个请求 多次alter
+      if (authorTipDoor) {
+        if (noAuthCode.includes(code)) {
+          noAuthDill()
+        } else {
+          // @ts-ignore
+          if (!res.config?.isNotTipErrorMsg) {
+            ElMessage.error({
+              message: msg,
+              duration: 2 * 1000
+            })
+          } else {
+            return res
+          }
+          return Promise.reject(msg)
+        }
       }
-      // @ts-ignore
-      if (!res.config?.isNotTipErrorMsg) {
-        ElMessage.error({
-          message: msg,
-          duration: 2 * 1000
-        })
-      }
-      return Promise.reject(msg)
     }
   },
   //响应报错
   (err) => {
-    useBasicStore().remotePromiseArrByReqUrl(tempReqUrlSave)  //从请求集合中移除
+    //取消请求
+    useBasicStore().remotePromiseArrByReqUrl(tempReqUrlSave)
     if (loadingInstance) {
       loadingInstance && loadingInstance.close()
     }
