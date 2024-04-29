fa28589075dc6f636b81e7e5dcfb10095af3ad15
kuanghua <869653722@qq.com>
2024-04-29 18:05:23 +0800

    fix common module issue when build the project

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
-import { viteMockServe } from 'vite-plugin-mock'
+// import { viteMockServe } from 'vite-plugin-mock'
 import Components from 'unplugin-vue-components/vite'
 import UnoCSS from 'unocss/vite'
 import { presetAttributify, presetIcons, presetUno } from 'unocss'
@@ -54,16 +54,16 @@ export default defineConfig(({ command, mode }) => {
         symbolId: 'icon-[dir]-[name]'
       }),
       //https://github.com/anncwb/vite-plugin-mock/blob/HEAD/README.zh_CN.md
-      viteMockServe({
-        enable:true,
-        mockPath: 'mock',
-        // prodEnabled: prodMock,
-        // injectCode: `
-        //   import { setupProdMockServer } from './mock-prod-server';
-        //   setupProdMockServer();
-        // `,
-        logger: true
-      }),
+      // viteMockServe({
+      //   enable:true,
+      //   mockPath: 'mock',
+      //   // prodEnabled: prodMock,
+      //   // injectCode: `
+      //   //   import { setupProdMockServer } from './mock-prod-server';
+      //   //   setupProdMockServer();
+      //   // `,
+      //   logger: true
+      // }),
       // VueSetupExtend(),using  DefineOptions instant of it
       //https://github.com/antfu/unplugin-auto-import/blob/HEAD/src/types.ts
       Components({
